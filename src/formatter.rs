097d14972bd0c508bd6c8960a78d2de7e6c9ba6a
Dave Stearns <dave.stearns@gmail.com>
2024-08-09 15:03:44 -0700

    docs for Formatter

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,15 +1,40 @@
 use rust_decimal::{Decimal, RoundingStrategy};
 
+/// Formats [Money] instances into Strings.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Formatter {
+    /// An explicit number of decimal places to round to and display.
+    /// If this is None, the [Currency::minor_units] will be used.
+    /// This defaults to None.
     pub decimal_places: Option<u32>,
+    /// The RoundingStrategy to use when rounding is necessary. This
+    /// defaults to RoundingStrategy::MidpointNearestEven
+    /// (aka "Banker's Rounding").
     pub rounding_strategy: RoundingStrategy,
+    /// The characters to use between the whole and fractional parts
+    /// of the amount. This defaults to ".".
     pub decimal_separator: &'static str,
-    pub digit_groupings: &'static [usize],
+    /// The digit grouping size. This defaults to 3.
+    /// Digits in the whole portion will be grouped by this size,
+    /// no matter how many there are.
+    pub digit_grouping: usize,
+    /// Custom digit groupings, expressed in right-to-left order.
+    /// For example, `&[3,2,2]` will group the right-most 3 digits,
+    /// then the 2 digits to the left of those, then the 2 digits
+    /// to the left of those, and then display any remaining digits
+    /// without any grouping. This defaults to None.
+    /// If specified, these override `digit_grouping`.
+    pub digit_groupings: Option<&'static [usize]>,
+    /// The characters to use between digit groups. This
+    /// defaults to ",".
     pub digit_group_separator: &'static str,
-    pub positive_format: &'static str,
-    pub negative_format: &'static str,
-    pub zero_format: Option<&'static str>,
+    /// The format template to use for positive amounts.
+    pub positive_template: &'static str,
+    /// The format template to use for negative amounts.
+    pub negative_template: &'static str,
+    /// An optional format template to use for zero amounts. This defaults
+    /// to None. If None, the positive_format will be used instead.
+    pub zero_template: Option<&'static str>,
 }
 
 impl Default for Formatter {
@@ -18,16 +43,20 @@ impl Default for Formatter {
             decimal_places: None,
             rounding_strategy: RoundingStrategy::MidpointNearestEven,
             decimal_separator: ".",
-            digit_groupings: &[3, 3, 3, 3, 3],
+            digit_grouping: 3,
+            digit_groupings: None,
             digit_group_separator: ",",
-            positive_format: "{s}{a}",
-            negative_format: "-{s}{a}",
-            zero_format: None,
+            positive_template: "{s}{a}",
+            negative_template: "-{s}{a}",
+            zero_template: None,
         }
     }
 }
 
 impl Formatter {
+    /// Formats a Decimal amount. If `self.decimal_places` is `None` the
+    /// `default_decimal_places` will be used, which is typically the number
+    /// of minor units in the currency.
     pub fn format_amount(&self, amount: Decimal, default_decimal_places: u32) -> String {
         // round to the desired number of decimal places
         let dp = self.decimal_places.unwrap_or(default_decimal_places);
@@ -50,10 +79,19 @@ impl Formatter {
         let whole = split.next().expect("expected at least some whole digits");
         let maybe_frac = split.next();
 
-        // Break up the whole digits according to the digit_groupings
+        // Break up the whole digits according to the digit_groupings.
+        // If `digit_groupings` is None, create a temporary vector of
+        // enough `digit_grouping` values given the length of `whole`
+        let digit_groupings = if let Some(dgs) = self.digit_groupings {
+            dgs
+        } else {
+            &vec![self.digit_grouping; (whole.len() / self.digit_grouping) + 1]
+        };
+
         let mut groups: Vec<&str> = Vec::new();
         let mut group_start = whole.len();
-        for &group_len in self.digit_groupings {
+
+        for &group_len in digit_groupings {
             if group_len >= group_start {
                 break;
             }
@@ -159,7 +197,7 @@ mod tests {
     #[test]
     fn format_amount_custom_digit_grouping() {
         let formatter = Formatter {
-            digit_groupings: &[3, 2, 2],
+            digit_groupings: Some(&[3, 2, 2]),
             ..Default::default()
         };
         assert_eq!(
@@ -171,7 +209,7 @@ mod tests {
     #[test]
     fn format_amount_no_digit_grouping() {
         let formatter = Formatter {
-            digit_groupings: &[],
+            digit_groupings: Some(&[]),
             ..Default::default()
         };
         assert_eq!(
