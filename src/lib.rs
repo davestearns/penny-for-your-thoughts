22d04c8d0ee91ea207e27981d60e6d99523fd128
Dave Stearns <dave.stearns@gmail.com>
2026-06-28 10:14:37 -0700

    add new version summary to crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -110,6 +110,11 @@
 //!
 //! ## Changes from Previous Versions
 //!
+//! ### Version 3.0.0 -> 3.1.0
+//! - `iso_currencies::find()` can be used to find the `Currency`
+//!    implementation for a given ISO currency code, or `None`
+//!    if no currency is defined for that code.
+//!
 //! ### Version 2.0.0 -> 3.0.0
 //! - The `round()` method now rounds the amount to the
 //!   currency's number of minor units by default.
